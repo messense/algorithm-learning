8460ce4fe1f102a403aa2504c163f474a00fc5b1
messense <wapdevelop@gmail.com>
2013-04-24 13:12:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithm Learning
+
+This repo contains codes I wrote when I was learning algorithm. These codes are all written in C. And I compiled and tested them on OS X with clang/gcc.
---
+++ src/insert_sort.c
@@ -0,0 +1,43 @@
+/**
+ * Insertion Sort 插入排序
+ * http://en.wikipedia.org/wiki/Insertion_sort
+ * http://zh.wikipedia.org/zh-cn/插入排序
+ */
+#include <stdio.h>
+
+void display(int a[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void insert_sort(int a[], int n) {
+    int i, tmp;
+    for (i = 1; i < n; i++) {
+        /* insertion sort begins at index 1 */
+        tmp = a[i];
+        int j = i - 1;
+        while (j >= 0 && tmp < a[j]) {
+            // a[i] < a[j] then insert a[j] after a[i]
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = tmp;
+        display(a, n);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int a[100];
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    insert_sort(a, n);
+    display(a, n);
+    return 0;
+}
